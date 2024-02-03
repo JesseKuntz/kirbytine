273f450b28bd3fd0471beaaad20c76249b25e02a
JesseKuntz <jessekuntz96@gmail.com>
2024-02-03 00:12:01 -0500

    feat: add kirby images

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,9 @@
 import type { MetaFunction } from "@remix-run/node";
 
+import kirbyHeart from "./images/kirby-heart.png";
+import kirbyChef from "./images/chef-kirby.png";
+import kirbySleepy from "./images/sleepy-kirby.webp";
+
 export const meta: MetaFunction = () => {
   return [
     { title: "Kirbytine" },
@@ -10,10 +14,26 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="p-10 flex flex-col items-center text-2xl">
-      <h1 className="text-primary text-8xl font-bold tracking-tighter">
-        Kirbytine
-      </h1>
-      <div className="rounded-lg bg-tertiary shadow-lg max-w-md min-h-40 p-8 mt-10">
+      <div className="flex items-center gap-6">
+        <h1 className="text-primary text-8xl font-bold tracking-tighter">
+          Kirbytine
+        </h1>
+        <img src={kirbyHeart} alt="Kirby holding a heart." width={80} />
+      </div>
+
+      <div className="rounded-lg border-[#e791bf] border-4 bg-tertiary shadow-lg text-center max-w-lg p-8 mt-10 relative">
+        <img
+          className="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2"
+          src={kirbySleepy}
+          alt="Sleepy Kirby."
+          width={80}
+        />
+        <img
+          className="absolute bottom-0 right-0 translate-x-1/2 translate-y-1/2"
+          src={kirbyChef}
+          alt="Chef Kirby."
+          width={80}
+        />
         Roses are red, Kirby is pink. Happy Kirbytine&apos;s day!
       </div>
     </div>
